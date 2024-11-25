5ffea877662c039abd45b95e0dbed15c1d2ec176
ZAHRA-MRAH <zahramohd198@gmail.com>
2024-11-25 22:56:54 +0300

    fixed typo

--- Backend/homepage.php
+++ Backend/homepage.php
@@ -86,7 +86,7 @@ if (isset($_SESSION['user_id'])) {
                     <form action="" method="post">
               <div class="dropdown">
                   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class"RoomType>Room Type</span>
+                    <span class="RoomType">Room Type</span>
                   </button>
                   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <a class="dropdown-item" href="#">Classroom</a>
@@ -96,8 +96,8 @@ if (isset($_SESSION['user_id'])) {
                 </div>
                 
   
-                <div clas="Date">
-                  <span class"Date>Pick a Date</span>
+                <div class="Date">
+                  <span class ="Date">Pick a Date</span>
                   <input type="date" name="Date" value="yyyy-mm-dd">
                 </div>
   
@@ -107,7 +107,7 @@ if (isset($_SESSION['user_id'])) {
             </div>
         </main>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6SpejpU02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6SpejpU02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
