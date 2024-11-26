29db025ae5aa6501b90663d87f3970c898b2a446
fmabd <fmabdulla229@gmail.com>
2024-11-26 03:17:44 +0300

    Fixed dropdown menu+ added a try catch block for the welcome messege

--- Backend/homepage.php
+++ Backend/homepage.php
@@ -30,7 +30,9 @@ if (isset($_SESSION['user_id'])) {
     <title>Home</title>
     <link rel="stylesheet" href="../Frontend/homestyle.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+    <script src="index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -73,21 +75,32 @@ if (isset($_SESSION['user_id'])) {
 </nav>
     </header>
         <main>
-            <h2 id="welcome-messege">Welcome <?php echo htmlspecialchars($fname)?>!</h2>
+            <h2 id="welcome-messege"><?php
+              try {
+                  if (!isset($fname)) {
+                      throw new Exception("Name not set");
+                  }
+
+                  echo "<h2 id='welcome-messege'>Welcome " . htmlspecialchars($fname) . "!</h2>";
+              } catch (Exception $e) {
+
+                  echo "<h2 id='welcome-messege'>Welcome Guest!</h2>";
+              }
+              ?></h2>
               <div class="container">
                   <div class="box form-box">
-                    <header>Search for Available Rooms:</header>
+                    <header>Search forr Available Rooms:</header>
                     <form action="" method="post">
-              <div class="dropdown">
-                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class"RoomType>Room Type</span>
-                  </button>
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Classroom</a>
-                    <a class="dropdown-item" href="#">Lab</a>
-                    <a class="dropdown-item" href="#">Meeting Room</a>
-                  </div>
-                </div>
+                    <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" id="dropdownMenuButton" data-bs-toggle="dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="true">
+                      Room Type
+                    </button>
+                      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><a href="" class="dropdown-item" onclick="show('Classroom')">Classroom</a></li>
+                        <li><a href="" class="dropdown-item" onclick="show('Computer Lab')">Computer Lab</a></li>
+                        <li><a href="" class="dropdown-item" onclick="show('Meeting Room')">Meeting Room</a></li>
+                      </ul>
+                    </div>
                 
   
                 <div clas="Date">
@@ -100,8 +113,6 @@ if (isset($_SESSION['user_id'])) {
           </div>
             </div>
         </main>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6SpejpU02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 </html>
