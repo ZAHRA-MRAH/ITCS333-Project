a52915411f9a8f73e5d7e693efbfb646723d5340
Bushra-11 <bu2004hu@gmail.com>
2024-11-30 00:27:36 +0300

    re-naming

--- Backend/viewBookings.php
+++ Backend/viewBookings.php
@@ -49,7 +49,7 @@ $result = $stmt->get_result();
     }
     ?>
 </div>
-<script src="js/scripts.js"></script> <!-- Link to external JS file -->
+<script src="js/cancelBooking.js"></script> <!-- Link to external JS file -->
 
 </body>
 </html>
