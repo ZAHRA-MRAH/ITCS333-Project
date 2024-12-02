9aff191761f67ff544d205b451f3188b6813c0f9
ZAHRA-MRAH <zahramohd198@gmail.com>
2024-12-02 19:33:39 +0300

    modify

--- Backend/booking.php
+++ Backend/booking.php
@@ -14,8 +14,7 @@ $equipment = $_POST['Equipment'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="booking.css">
-    <link rel="stylesheet" href="../Frontend/style.css">
+
 
     
     <title>Booking</title>
