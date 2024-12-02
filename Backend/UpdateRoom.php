66b982f70d3e47b53c2aa83d1b1015ac0edced2e
Batool Sayed Ebrahim <202109262@stu.uob.edu.bh>
2024-12-02 17:21:22 +0300

    edit

--- Backend/UpdateRoom.php
+++ Backend/UpdateRoom.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../Frontend/AdminStyle.css">
+    <script src="../Frontend/updateRoom.js"> </script>
     <title>Update Room</title>
 </head>
 <body>
