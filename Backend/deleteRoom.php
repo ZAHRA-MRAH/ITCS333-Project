103d3973f889e1d33ac9daf43a6badfbddbffa6d
Batool Sayed Ebrahim <202109262@stu.uob.edu.bh>
2024-12-02 17:21:16 +0300

    edit

--- Backend/deleteRoom.php
+++ Backend/deleteRoom.php
@@ -61,6 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../Frontend/AdminStyle.css">
+    <script src="../Frontend/deleteRoom.js"> </script>
     <title>Delete Room</title>
 </head>
 <body>
