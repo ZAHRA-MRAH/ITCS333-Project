97fdc39deb9a1e1c576cb085c6f0cad601a8a712
fmabd <fmabdulla229@gmail.com>
2024-12-09 20:22:33 +0300

    fixed profile box+update admin styling

--- Backend/AdminPanel.php
+++ Backend/AdminPanel.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
@@ -75,6 +76,8 @@ if (!isset($_SESSION['user_id'])) {
             </form>
         </div>
     </main>
+
+
 </body>
 
 </html>
